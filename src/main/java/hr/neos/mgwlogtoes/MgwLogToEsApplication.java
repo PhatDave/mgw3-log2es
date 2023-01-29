491bb81b814ec2be3514d8cb116e00220db393b2
David Majdandžić <dmajdandzic@neos.hr>
2023-01-29 16:56:38 +0100

    Initial commit

---
+++ src/main/java/hr/neos/mgwlogtoes/MgwLogToEsApplication.java
@@ -0,0 +1,13 @@
+package hr.neos.mgwlogtoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgwLogToEsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgwLogToEsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/neos/mgwlogtoes/MgwLogToEsApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.neos.mgwlogtoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgwLogToEsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
